f7676658a429404e7a8a88130dd2cdcb4cc8f451
Syafwan000 <muhammadsyafwan04@gmail.com>
2025-02-26 15:00:35 +0700

    feat: configure manual chunking in Rollup for optimized build output

--- vite.config.ts
+++ vite.config.ts
@@ -11,5 +11,16 @@ export default defineConfig({
       { find: '@pages', replacement: path.resolve(__dirname, 'src/pages') },
       { find: '@components', replacement: path.resolve(__dirname, 'src/components') }
     ]
-  }
+  },
+  build: {
+    rollupOptions: {
+        output: {
+            manualChunks(id) {
+                if (id.includes('node_modules')) {
+                    return id.toString().split('node_modules/')[1].split('/')[0].toString();
+                }
+            }
+        }
+    }
+}
 })
